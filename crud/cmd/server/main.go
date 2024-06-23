0d532ea77b44d59ce6c0f8265d2e5823a72309b8
drawiin <vini65599@gmail.com>
2024-06-23 14:05:45 -0400

    refactor: Update user handler to use JWT authentication

--- crud/cmd/server/main.go
+++ crud/cmd/server/main.go
@@ -5,16 +5,36 @@ import (
 	"net/http"
 
 	"github.com/drawiin/go-expert/crud/config"
+	_ "github.com/drawiin/go-expert/crud/docs"
 	"github.com/drawiin/go-expert/crud/internal/entity"
 	"github.com/drawiin/go-expert/crud/internal/infra/database"
 	"github.com/drawiin/go-expert/crud/internal/infra/webserver/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+// @title Go Expert CRUD API
+// @version 1.0
+// @description Product API with authentication
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://swagger.io/support
+// @contact.email email@email.com
+
+// @license.name MIT
+// @license.url http://opensource.org/licenses/MIT
+
+// @host localhost:8080
+// @BasePath /
+// @schemes http
+// @securityDefinitions.apiKey jwt
+// @in header
+// @name Authorization
 func main() {
 	// LoadConfig function is called to load the configuration values from the .env file
 	config, err := config.LoadConfig(".")
@@ -51,10 +71,12 @@ func main() {
 		r.Post("/seed", productHandler.Seed)
 	})
 
-
 	r.Post("/users/signup", userHandler.CreateUser)
 	r.Post("/users/seed", userHandler.Seed)
 	r.Post("/users/login", userHandler.Login)
+
+	// Aparently the doc.json in the end is not optional
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8080/docs/doc.json")))
 
 	http.ListenAndServe(":8080", r)
 }
