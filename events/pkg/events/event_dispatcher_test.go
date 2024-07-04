11550fe9e71819e01fe977e69ded8511545b0177
drawiin <vini65599@gmail.com>
2024-07-04 08:26:32 -0400

    refactor: Improve channel handling in main.go

---
+++ events/pkg/events/event_dispatcher_test.go
@@ -0,0 +1,67 @@
+package events
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type TestEvent struct {
+	Name    string
+	Payload interface{}
+}
+
+func (e *TestEvent) GetName() string {
+	return e.Name
+}
+
+func (e *TestEvent) GetPayload() interface{} {
+	return e.Payload
+}
+
+func (e *TestEvent) GetDateTime() time.Time {
+	return time.Now()
+}
+
+type TestEventHandler struct{}
+
+func (h *TestEventHandler) Handle(event EventInterface) {
+	// do nothing
+}
+
+type EventDispatcherTestSuite struct {
+	suite.Suite
+	event           TestEvent
+	event2          TestEvent
+	handler         TestEventHandler
+	handler2        TestEventHandler
+	handler3        TestEventHandler
+	eventDispatcher *EventDispatcher
+}
+
+func (suite *EventDispatcherTestSuite) SetupTest() {
+	suite.event = TestEvent{Name: "test", Payload: "test"}
+	suite.event2 = TestEvent{Name: "test2", Payload: "test2"}
+	suite.handler = TestEventHandler{}
+	suite.handler2 = TestEventHandler{}
+	suite.handler3 = TestEventHandler{}
+	suite.eventDispatcher = NewEventDispatcher()
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register() {
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)
+	suite.Nil(err)
+	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	suite.Equal(&suite.handler, suite.eventDispatcher.handlers[suite.event.GetName()][0])
+	suite.Equal(&suite.handler2, suite.eventDispatcher.handlers[suite.event.GetName()][1])
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(EventDispatcherTestSuite))
+}
