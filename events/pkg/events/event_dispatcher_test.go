3d7f4db93ab15e1c6f72a6dedf075a005dc726b9
drawiin <vini65599@gmail.com>
2024-07-04 20:43:21 -0400

    refactor: Remove event handler from event_dispatcher.go
    This commit removes the `Remove` method from the `EventDispatcher` struct in the `event_dispatcher.go` file. The method was previously added but is no longer needed. This change simplifies the codebase by removing unnecessary functionality.

--- events/pkg/events/event_dispatcher_test.go
+++ events/pkg/events/event_dispatcher_test.go
@@ -118,6 +118,18 @@ func (suite *EventDispatcherTestSuite) TestEventDispatcher_Dispatch() {
 	eh.AssertNumberOfCalls(suite.T(), "Handle", 1)
 }
 
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Remove() {
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
+	suite.Nil(err)
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)
+	suite.Nil(err)
+
+	err = suite.eventDispatcher.Remove(suite.event.GetName(), &suite.handler)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+	suite.Equal(&suite.handler2, suite.eventDispatcher.handlers[suite.event.GetName()][0])
+}
+
 func TestSuite(t *testing.T) {
 	suite.Run(t, new(EventDispatcherTestSuite))
 }
