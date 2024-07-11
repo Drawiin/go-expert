6f4ac33cb8a279d96b7640563a72cdc786c7750d
drawiin <vini65599@gmail.com>
2024-07-11 12:21:46 -0400

    refactor: Add CreateCategoryStream and CreateCategoryStreamBidirectional methods to CategoryService

--- grpc/internal/service/category_service.go
+++ grpc/internal/service/category_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/drawiin/go-expert/grpc/internal/database"
 	"github.com/drawiin/go-expert/grpc/internal/pb"
@@ -28,6 +29,50 @@ func (s *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 	}, nil
 }
 
+func (s *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			stream.SendAndClose(categories)
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		createdCategory, err := s.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          createdCategory.ID,
+			Name:        createdCategory.Name,
+			Description: createdCategory.Description,
+		})
+	}
+}
+
+func (s *CategoryService) CreateCategoryStreamBidirectional(stream pb.CategoryService_CreateCategoryStreamBidirectionalServer) error {
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		createdCategory, err := s.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+		stream.Send(&pb.Category{
+			Id:          createdCategory.ID,
+			Name:        createdCategory.Name,
+			Description: createdCategory.Description,
+		})
+	}
+}
+
 func (s *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
 	categories, err := s.CategoryDB.GetAll()
 	if err != nil {
@@ -42,4 +87,16 @@ func (s *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb
 		})
 	}
 	return &pb.CategoryList{Categories: pbCategories}, nil
+}
+
+func (s *CategoryService) GetCategoryById(ctx context.Context, in *pb.GetCategoryByIdRequest) (*pb.Category, error) {
+	category, err := s.CategoryDB.GetById(in.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}, nil
 }
